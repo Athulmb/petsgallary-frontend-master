b48dc9c54c8f47b322be7298727b2785ed266be3
Athulmb <athulmb66@gmail.com>
2025-07-31 21:15:49 +0530

    last commit

--- src/Components/StorePage.js
+++ src/Components/StorePage.js
@@ -553,7 +553,7 @@ const StorePage = () => {
               </div>
 
               {/* Product grid */}
-              <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
+              <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5  gap-6">
                 {filteredProducts.map(product => (
                   <ProductCard key={product.id} product={product} />
                 ))}
